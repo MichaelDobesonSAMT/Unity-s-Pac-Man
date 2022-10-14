07c9f28f7f733103d7f7afcc12848de8f7c50550
Michael Yorke Dobeson <michael.dobeson@CPT.local>
2022-10-14 08:41:28 +0200

    2022-10-07

---
+++ 5_Sito o applicativo/Progetto/Unity's Pac-Man/Assets/Scripts/GridManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public float[,] Grid;
+    int Vertical, Horizontal, Columns, Rows;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vertical = (int)Camera.main.orthographicSize;
+        Horizontal = Vertical *(Screen.width / Screen.height);
+        Columns = Horizontal * 2;
+        Rows = Vertical * 2;
+        Grid = new float[Columns, Rows];
+        for (int i = 0; i < Columns; i++)
+        {
+            for (int j = 0; j < Rows; j++)
+            {
+                Grid[i, j] = Random.Range(0.0f, 1.0f);
+                SpawnTile(i, j, Grid[i, j]);  
+            }
+        }
+    }
+
+    private void SpawnTile(int x, int y, float value)
+    {
+        GameObject g = new GameObject("x: " + x + ",y: " + y);
+        g.transform.position = new Vector3(x - (Horizontal - 0.5f), y - (Vertical - 0.5f));
+        var s = g.AddComponent<SpriteRenderer>();
+        s.color = new Color(value, value, value);
+    }
+}
