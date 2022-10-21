cbd1854b3707cc4f7cd48fc849de8dd54f08302e
Michael Yorke Dobeson <michael.dobeson@CPT.local>
2022-10-21 09:02:52 +0200

    2022-10-14

--- 5_Sito o applicativo/Progetto/Unity's Pac-Man/Assets/Scripts/GridManager.cs
+++ 5_Sito o applicativo/Progetto/Unity's Pac-Man/Assets/Scripts/GridManager.cs
@@ -14,19 +14,21 @@ public class GridManager : MonoBehaviour
     public float[,] Grid;
     public bool[,] GridWalls;
     public float WallPercent = 0.3f;
-    int Vertical, Horizontal, Columns, Rows;
+    static int Vertical, Horizontal, Columns, Rows;
     
     void Start()
     {
+        // Set Sizes
         Horizontal = Vertical = (int)Camera.main.orthographicSize * 2;
         Columns = Rows = Horizontal * 2;
         Grid = new float[Columns, Rows];
         GridWalls = new bool[Columns, Rows];
 
+        // Place Walls
         for (int i = 0; i < Columns; i++)
         {
             for (int j = 0; j < Rows; j++)
-            {/*
+            {
                 var rnd = Random.Range(0.0f, 1.0f);
                 if(rnd < WallPercent){
                     GridWalls[i,j] = true;
@@ -40,25 +42,21 @@ public class GridManager : MonoBehaviour
                     }
                 }else{
                     GridWalls[i,j] = false;
-                }*/
-                GridWalls[1, 0] = false;
-                GridWalls[1, 5] = false;
-                GridWalls[2, 5] = false;
-                GridWalls[3, 5] = false;
-                GridWalls[3, 6] = false;
-                SetGrid();
+                }
             }
         }
-        Visit(Grid, 0, 0, 0);
+
+        // Place Tiles
         for (int i = 0; i < Columns; i++)
         {
-            for (int j = 0; j < Rows; j++){
+            for (int j = 0; j < Rows; j++)
+            {
                 SpawnTile(i, j, Grid[i, j]);
-                Debug.Log("C: " + i + " ,R: " + j + " ,val: " + Grid[i,j]);
             }
         }
     }
 
+    // Imposta la mappa
     private void SetGrid(){
         Grid = new float[Columns, Rows];
 
@@ -71,25 +69,26 @@ public class GridManager : MonoBehaviour
                 }
             }
         }
-        Visit(Grid, 0, 0, 0); //2,3 parameters are for pac pos
+        Visit(Grid, 0, 0, 0);
     }
 
-    //manhattan distance algorithm
-    private void Visit(float[,] tempGrid, int col, int row, int dist){
+    // Manhattan distance algorithm
+    public static void Visit(float[,] tempGrid, int col, int row, int dist){
         if(row < Rows && row >= 0 && col < Columns && col >= 0){
             if(
                 (tempGrid[col,row] > dist && tempGrid[col,row] != WALL) || 
                 tempGrid[col,row] == INVALID
             ){
                 tempGrid[col,row] = dist;
-                Visit(tempGrid, row, col-1,dist+1);
-                Visit(tempGrid, row-1, col,dist+1);
-                Visit(tempGrid, row, col+1,dist+1);
-                Visit(tempGrid, row+1, col,dist+1);
+                Visit(tempGrid, col, row-1,dist+1);
+                Visit(tempGrid, col-1, row, dist+1);
+                Visit(tempGrid, col, row+1,dist+1);
+                Visit(tempGrid, col+1, row, dist+1);
             }
         }
     }
     
+    // Spawns an element of the map
     private void SpawnTile(int x, int y, float value)
     {
         GameObject g = new GameObject("x: " + x + ",y: " + y);
@@ -101,12 +100,17 @@ public class GridManager : MonoBehaviour
         }else{
             s.color = new Color(0, 0, 0);
         }
+
+        // Adds text for debug
+        /*
         var myText = CreateText(g.transform);
         myText.text = x + ", " + y + "\n: " + value;
-        myText.transform.position = new Vector3(x - (Horizontal), (Vertical) - y, -0.25f);
+        myText.transform.position = new Vector3(x - (Horizontal), (Vertical) - y, -0.0f);
         myText.transform.localScale = new Vector3(0.25f, 0.25f);
+        */
     }
 
+    // Adds text to a Game Object
     private TextMesh CreateText(Transform parent)
     {
         var go = new GameObject();
